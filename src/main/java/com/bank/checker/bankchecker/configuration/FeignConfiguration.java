d1019dc36b8f4bb681cf3a29cb155c973d7014bc
Petr Kukrál <p.kukral.pro@gmail.com>
2019-01-06 15:50:36 +0100

    New project

---
+++ src/main/java/com/bank/checker/bankchecker/configuration/FeignConfiguration.java
@@ -0,0 +1,9 @@
+package com.bank.checker.bankchecker.configuration;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class FeignConfiguration {
+}
---
+++ src/test/java/com/bank/checker/bankchecker/BankCheckerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bank.checker.bankchecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankCheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
