7ce70da6c04cb8045a120dd7391698e4ef8f5912
Petr Kukrál <p.kukral.pro@gmail.com>
2019-01-13 14:08:36 +0100

    Basic authentication

--- src/main/java/com/bank/checker/bankchecker/controller/AppController.java
+++ src/main/java/com/bank/checker/bankchecker/controller/AppController.java
@@ -7,10 +7,12 @@ import com.bank.checker.bankchecker.service.mapper.FioTranstactionsMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController(value = "/api")
+@RestController
+@RequestMapping(value = "/api")
 public class AppController {
 
     @Autowired
