fa86e47ba41f36bbe2c96f7766308d3027f580d0
Petr Kukrál <p.kukral.pro@gmail.com>
2019-01-13 15:21:52 +0100

    Basic authentication - fixed and moved some configuration to the config

--- src/main/java/com/bank/checker/bankchecker/security/WebSecurityConfig.java
+++ src/main/java/com/bank/checker/bankchecker/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.bank.checker.bankchecker.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,27 +14,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${security.users.username}")
+    private String userName;
+
+    @Value("${security.users.userpassword}")
+    private String userPassword;
+
+    @Value("${security.users.adminname}")
+    private String adminName;
+
+    @Value("${security.users.adminpassword}")
+    private String adminPassword;
+
+    private final String ROLE_USER = "USER";
+    private final String ROLE_ADMIN = "ADMIN";
+
     // For users and roles
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("admin")
-                    .password(passwordEncoder().encode("adminPass"))
-                    .roles("ADMIN")
+                .withUser(adminName)
+                    .password(passwordEncoder().encode(adminPassword))
+                    .roles(ROLE_ADMIN)
                 .and()
-                .withUser("user")
-                    .password(passwordEncoder().encode("userPass"))
-                    .roles("USER");
+                .withUser(userName)
+                    .password(passwordEncoder().encode(userPassword))
+                    .roles(ROLE_USER);
     }
 
     // Url restrictions
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                //.antMatchers("/getTransactions").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/actuator/**").hasRole(ROLE_ADMIN)
+            .antMatchers("/api/**").hasAnyRole(ROLE_USER, ROLE_ADMIN)
+            .and()
+            .httpBasic();
     }
 
     @Bean
